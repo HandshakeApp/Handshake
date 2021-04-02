cd2590b262bdaa7e5d26997d6eb8a4e22f25feb4
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2021-04-02 10:02:31 +0100

    create AppRoutes component

---
+++ src/components/AppRoutes/index.tsx
@@ -0,0 +1,24 @@
+import { Redirect, Route } from "react-router-dom";
+import { useAuth } from "../../auth";
+import Home from "../../pages/Home";
+
+
+const AppMenuItems: React.FC = () => {
+    const { loggedIn } = useAuth();
+	if(!loggedIn) {
+		return (
+			<Redirect to="/auth/login"/>
+		)
+	}
+    return(
+        <>
+            <Route path="/Home" exact={true}>
+                <Home/>
+            </Route>
+            <Route path="/:name" exact={true}>
+            </Route>
+        </>
+    );
+};
+
+export default AppMenuItems;
