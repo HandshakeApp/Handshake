a7d3e20a922122ac60551548e834d163c615ad12
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2021-04-02 14:15:47 +0100

    create login function

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -1,7 +1,19 @@
 import React, { useContext } from 'react';
+import { app } from "../util/mongodb";
 
 export const AuthContext = React.createContext({ loggedIn: false});
 
 export function useAuth() {
     return useContext(AuthContext);
+};
+
+export async function login(email: string, password: string) {
+    const credentials = Realm.Credentials.emailPassword(email, password);
+
+    try {
+        const user: Realm.User = await app.logIn(credentials);
+        return user;
+    } catch(err) {
+        console.log(err);
+    }
 };
