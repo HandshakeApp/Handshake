98ade749994ad5244bdd1f01694e178c60ecbddb
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2021-04-02 10:02:49 +0100

    use props

--- src/pages/Page/index.tsx
+++ src/pages/Page/index.tsx
@@ -1,13 +1,10 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { useParams } from 'react-router';
 import './Page.css';
 
 interface Props {
 	name: string;
 }
 const Page: React.FC<Props> = (props) => {
-
-	const { name } = useParams<Props>();
 
 	return (
 		<IonPage>
@@ -16,14 +13,14 @@ const Page: React.FC<Props> = (props) => {
 					<IonButtons slot="start">
 						<IonMenuButton />
 					</IonButtons>
-					<IonTitle>{name}</IonTitle>
+					<IonTitle>{props.name}</IonTitle>
 				</IonToolbar>
 			</IonHeader>
 
 			<IonContent fullscreen>
 				<IonHeader collapse="condense">
 					<IonToolbar>
-						<IonTitle size="large">{name}</IonTitle>
+						<IonTitle size="large">{props.name}</IonTitle>
 					</IonToolbar>
 				</IonHeader>
 				{props.children}
