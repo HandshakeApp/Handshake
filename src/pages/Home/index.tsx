9090c514d52d7dd7662967a318809360c5d62b22
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2021-04-02 09:53:17 +0100

    create Home component

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import Page from "../Page";
+
+
+const Home: React.FC = () => {
+    console.log("HOERMFSD")
+    return(
+        <Page name="Home">
+            aweslgdfugek
+        </Page>
+    );
+};
+
+export default Home;
