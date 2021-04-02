f195f5cc3082b5d15141524d0aa7849fc18bdd27
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2021-04-02 10:02:42 +0100

    use auth dummy

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,23 +1,42 @@
-import { IonItem, IonInput } from '@ionic/react';
+import { IonItem, IonInput, IonContent, IonButton } from '@ionic/react';
 import { useState } from 'react';
+import { Redirect } from "react-router";
+import Page from "../Page";
+import { useAuth } from "../../auth";
 
-const Login: React.FC = () => {
+interface Props {
+    onLogin: () => void;
+}
+
+const Login: React.FC<Props> = ({ onLogin }) => {
     const [email, setEmail] = useState<string>();
     const [password, setPassword] = useState<string>();
 
+    const { loggedIn } = useAuth();
+    if(loggedIn) {
+        return <Redirect to="/Home"/>
+    }
+
     return(
-        <form>
-            <IonItem>
-                <IonInput value={email} placeholder="Enter email" 
-                    onIonChange={e => setEmail(e.detail.value!)}>
-                </IonInput>
-            </IonItem>
-            <IonItem>
-                <IonInput type="password" value={password} placeholder="Enter password" 
-                    onIonChange={e => setPassword(e.detail.value!)}>
-                </IonInput>
-            </IonItem>
-        </form>
+        <Page name="Login">
+            <IonContent className="ion-padding">
+                <form>
+                    <IonItem>
+                        <IonInput value={email} placeholder="Enter email" 
+                            onIonChange={e => setEmail(e.detail.value!)}>
+                        </IonInput>
+                    </IonItem>
+                    <IonItem>
+                        <IonInput type="password" value={password} placeholder="Enter password" 
+                            onIonChange={e => setPassword(e.detail.value!)}>
+                        </IonInput>
+                    </IonItem>
+                    <IonButton expand="block" onClick={onLogin}>
+                        Login
+                    </IonButton>
+                </form>
+            </IonContent>
+        </Page>
         
     );
 };
