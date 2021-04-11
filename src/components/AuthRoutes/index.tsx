3c06f8a17e8fffa3b0bde74432cd4607f96f2a5f
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2021-04-11 07:44:47 +0100

    add logout route

--- src/components/AuthRoutes/index.tsx
+++ src/components/AuthRoutes/index.tsx
@@ -1,12 +1,18 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import Login from "../../pages/Login";
+import Logout from "../../pages/Logout";
 
 const AuthRoutes: React.FC = () => {
     return(
-        <Route path="/auth/login" exact>
-            <Login />
-        </Route>
+        <Switch>
+            <Route path="/auth/login" exact>
+                <Login />
+            </Route>
+            <Route path="/auth/logout" exact>
+                <Logout />
+            </Route>
+        </Switch>
     );
     
 }
